a8e3b83c35d9b4e5677354ec7e074ae582eb5ed5
MIR TAUHIDUL ISLAM <mirtauhidulislam@MIRs-MacBook-Air.local>
2021-08-02 21:08:20 -0700

    OK

---
+++ src/style/Global.tsx
@@ -0,0 +1,25 @@
+// eslint-disable-next-line import/no-unresolved
+
+import { SaltTheme } from '@saltswap/uikit'
+import { createGlobalStyle } from 'styled-components'
+
+declare module 'styled-components' {
+  /* eslint-disable @typescript-eslint/no-empty-interface */
+  export interface DefaultTheme extends SaltTheme {}
+}
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    font-family: 'Kanit', sans-serif;
+  }
+  body {
+    background-color: ${({ theme }) => theme.colors.background};
+
+    img {
+      height: auto;
+      max-width: 100%;
+    }
+  }
+`
+
+export default GlobalStyle
